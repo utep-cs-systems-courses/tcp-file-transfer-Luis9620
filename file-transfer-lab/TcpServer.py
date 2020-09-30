4862b73122a9d1045f4932c4419856303b007ce5
Luis9620 <lasotomorales@miners.utep.edu>
2020-09-30 15:18:30 -0400

    Adding the binding to the socket

--- file-transfer-lab/TcpServer.py
+++ file-transfer-lab/TcpServer.py
@@ -2,8 +2,10 @@
 import sys
 import re
 import socket
+sys.path.append("../lib")       # for params
 import params
 import os
+from FramedSock import framed_send, framed_receive
 
 switchesVarDefaults = (
     (('-l', '--listenPort') ,'listenPort', 50001),
@@ -20,10 +22,26 @@ if paramMap['usage']:
 
 listener_socket = socket.socket(socket.AF_INET, socket.SOCK_STREAM)
 binding_address = ("127.0.0.1", listenPort)
-
+listener_socket.bind(binding_address)
+listener_socket(5)
+print("Listening on: ", binding_address)
 
 if __name__ == '__main__':
     while True:
         sock, address = listener_socket.accept()
+        while True:
+            payload = framed_receive(sock, debug)
+            if debug:
+                print("rec'd: ", payload)
+            if not payload:
+                break
+            payload += b"!"  # make emphatic!
+            framed_send(sock, payload, debug)
+
+            output_file = input("give me output: ")
+            output = open(output_file, 'w')
+            payload = payload.decode('utf8')
+            output.write(payload)
+
 
 
